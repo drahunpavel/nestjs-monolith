68e683d22ec75f74956c1329a932411805e5763d
Paul Drahun <drahunpavel@gmail.com>
2026-05-31 16:57:42 +0300

    prisma service and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { PrismaExceptionFilter } from './common/filters/prisma.filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); //  включает автоматическую проверку входящих данных на всех маршрутах приложения
+  app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
