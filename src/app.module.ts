e3a934304764efc64177b3bbea4e64bd095d81a8
Paul Drahun <drahunpavel@gmail.com>
2026-06-02 15:24:28 +0300

    add logger (pino)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      }
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
