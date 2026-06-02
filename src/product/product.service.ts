1c731de6b976851252cab76f2e81345b3a7cdcdc
Paul Drahun <drahunpavel@gmail.com>
2026-06-02 21:29:22 +0300

    implement CRUD operations in Brand modules

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { BaseResourceService } from 'src/common/base-resource/base.resource.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ProductService {}
+export class ProductService extends BaseResourceService {
+    constructor(prisma: PrismaService) {
+        super(prisma, 'product');
+    }
+}
