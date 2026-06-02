e73cbbfe4ea9c4dad5eb648249e39118e1ad40c2
Paul Drahun <drahunpavel@gmail.com>
2026-06-02 21:20:24 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaExceptionFilter } from './common/filters/prisma.filters';
 import { Logger } from 'nestjs-pino';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -12,6 +13,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe()); //  включает автоматическую проверку входящих данных на всех маршрутах приложения
   app.useGlobalFilters(new PrismaExceptionFilter());
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Monolith API')
+    .setDescription('REST API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/user/dto/create.user.dto.ts
+++ src/user/dto/create.user.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @IsEmail()
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({ example: 'test@example.com', required: true })
     email: string;
 
     @IsOptional()
     @IsString()
     @MinLength(8)
+    @ApiProperty({ example: 'password123', required: false })
     password?: string;
 }
