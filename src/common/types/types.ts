4280a3fe683bb26f6535b6f1ae7aff398bd3eadc
Paul Drahun <drahunpavel@gmail.com>
2026-06-04 22:30:14 +0300

    improve elasticsearch service

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -2,3 +2,12 @@ export type User = {
     id: number;
     email: string;
 }
+
+export type ProductWithBrand = {
+    id: number;
+    title: string;
+    description: string;
+    brand: {
+        title: string;
+    };
+}
