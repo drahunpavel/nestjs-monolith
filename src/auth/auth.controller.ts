d38a8fae334e9e4a0cc928136d40654f4c566a46
Paul Drahun <drahunpavel@gmail.com>
2026-06-04 12:47:54 +0300

    add local strategy and local guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local.auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  /**
+   * любое обрашение будет проходить через этот метод (quard)
+   * этот метод валидирует пользователя и возвращает его
+   */
+  @Post('login')
+  @UseGuards(LocalAuthGuard)
+  async login(@Req() req) {
+    // return this.authService.login(req.user);
+    return req.user;
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [UserModule],
+  providers: [AuthService, LocalStrategy],
+  imports: [UserModule, PassportModule],
 })
 export class AuthModule {}
