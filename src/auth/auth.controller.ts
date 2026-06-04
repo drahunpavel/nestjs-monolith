e06f02ed87e18e77605379e586027c2ea0358bca
Paul Drahun <drahunpavel@gmail.com>
2026-06-04 14:57:43 +0300

    add register endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local.auth.guard';
 import { JwtAuthGuard } from './guards/jwt.auth.guard';
+import { CreateUserDto } from 'src/user/dto/create.user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
 
   /**
    * любое обрашение будет проходить через этот метод (quard)
