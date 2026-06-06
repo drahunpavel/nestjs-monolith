78aefe0224f136537fbceee0729e36e6964dcb8e
Paul Drahun <drahunpavel@gmail.com>
2026-06-06 14:47:55 +0300

    improve elasticsearch controller

--- src/elastic/elastic.controller.ts
+++ src/elastic/elastic.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ElasticService } from './elastic.service';
 
 @Controller('elastic')
 export class ElasticController {
   constructor(private readonly elasticService: ElasticService) {}
+
+  @Post('index-all-products')
+  async indexAllProductsWithBrands() {
+    await this.elasticService.deleteIndexProductsWithBrands();
+    await this.elasticService.createIndexProductsWithBrands();
+    await this.elasticService.indexAllProductsWithBrands();
+    return { message: 'Success' };
+  }
+
+  @Get('indices')
+  async getIndices() {
+    return this.elasticService.getIndices();
+  }
+
+  @Get('search')
+  async searchProductsWithBrands(@Query('q') query: string) {
+    return this.elasticService.searchProductsWithBrands(query);
+  }
 }
