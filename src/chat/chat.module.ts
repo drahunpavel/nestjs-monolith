de2eb63a5785deb01712520a143a0b011a4f2241
Paul Drahun <drahunpavel@gmail.com>
2026-06-06 19:03:54 +0300

    integrate webSocket

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,10 +3,11 @@ import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Chat, ChatSchema } from './schemas/chat.schema';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }])],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway],
 })
 export class ChatModule {}
